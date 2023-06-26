a8657146fcff87178f0cfeaf3c8b7840f1eaa046
Arthur LITSCHIG <arthurlitschig@gmail.com>
2023-06-26 10:03:55 +0200

    AJOUT poissons

---
+++ frontend/app/Simulation/starting/validation.js
@@ -0,0 +1,31 @@
+/**
+ * Fonction de validation de l'ajout d'un poisson.
+ * Note : c'est une fonction JS pure, pas un composant React.
+ * 
+ * @param {*} p Poisson qu'on souhaite ajouter à la liste
+ * @param {*} listePoissons Liste de poissons
+ * @returns {boolean}
+ */
+
+export function validation(p, listePoissons) {
+    const isAgressiveFish = p.nom_comportement === 'agressif';
+    const hasOtherSpecies = listePoissons.some((poisson) => poisson.id !== p.id);
+    const hasMultipleAgressiveFish =
+      isAgressiveFish && listePoissons.filter((poisson) => poisson.id === p.id).length > 1;
+  
+    const espece = p.nom_commun;
+    const nombreExemplaires = listePoissons.reduce((total, poisson) => {
+      if (poisson.nom_commun === espece) {
+        return total + poisson.quantite;
+      }
+      return total;
+    }, 0);
+  
+    const pointsEspece = p.points * nombreExemplaires;
+  
+    const ajoutOk = (isAgressiveFish && hasOtherSpecies) || hasMultipleAgressiveFish;
+  
+    return ajoutOk;
+    
+  }
+  
