5d0b601d3c00b141ac6456a209b72e80a327e16f
Laurent DAVERIO <laurent@daverio.net>
2023-06-30 15:44:27 +0200

    Suppression d'exemples obsolètes

--- backend/old/queries.sql
+++
@@ -1,8 +0,0 @@
--- name: get_poissons
-SELECT * FROM poisson;
-
--- name: get_poissons_by_genre
-SELECT nom_scientifique
-FROM poisson, genre
-WHERE poisson.id_genre = genre.id 
-AND genre.nom like :genre;
--- backend/old/query_anodb.py
+++
@@ -1,9 +0,0 @@
-import anodb
-# parameters: driver, connection string, SQL file
-db = anodb.DB("mysqldb", 'localhost', "queries.sql",
-               options={'database': 'aquarius', 'user': 'aquarius','password':'aquAriuS'})
-
-for p in db.get_poissons_by_genre(genre='Tha%'):
-    print(p)
-
-db.close()
